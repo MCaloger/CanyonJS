fd5b861a6b7da86d6e4ae8882920cfe973f47251
Matthew Caloger <mattcaloger@gmail.com>
2020-08-22 15:46:02 -0400

    work to allow components

--- js/Components/CompletedTodos.js
+++ js/Components/CompletedTodos.js
@@ -1,45 +1,48 @@
+canyon.store.completedTodos = canyon.field("completedTodos", []);
 
-canyon.store.completedTodos = canyon.field("completedTodos", [])
+canyon.store.completedTodoCounter = canyon.field("completedTodoCounter", 0);
 
-canyon.store.completedTodoCounter = canyon.field("completedTodoCounter", 0)
+canyon.actions.uncompleteTodo = canyon.action(
+  "uncompleteTodo",
+  ["click"],
+  (e) => {
+    let listId = e.target.getAttribute("data-list-id");
+    let newList = canyon.store.completedTodos.get();
+    let newItem = newList.splice(listId, 1);
+    canyon.store.todos.set([...canyon.store.todos.get(), newItem].sort());
+    canyon.store.completedTodos.set(newList.sort());
+  }
+);
 
-canyon.actions.uncompleteTodo = canyon.action("uncompleteTodo", ["click"], (e) => {
-    let listId = e.target.getAttribute("data-list-id")
-    let newList = canyon.store.completedTodos.get()
-    let newItem = newList.splice(listId, 1)
-    canyon.store.todos.set([...canyon.store.todos.get(), newItem].sort())
-    canyon.store.completedTodos.set(newList.sort())
+canyon.actions.deleteTodo = canyon.action("deleteTodo", "click", (e) => {
+  let listId = e.target.getAttribute("data-list-id");
 
-    
-})
+  let newList = canyon.store.completedTodos.get();
+  newList.splice(listId, 1);
+  canyon.store.completedTodos.set(newList.sort());
+});
 
-canyon.actions.deleteTodo = canyon.action("deleteTodo", "click", e => {
-    let listId = e.target.getAttribute("data-list-id")
+canyon.watchers.completedTodoWatcher = canyon.watch(
+  [canyon.store.completedTodos],
+  () => {
+    let list = canyon.store.completedTodos.get();
+    canyon.store.completedTodoCounter.set(list.length);
 
-    let newList = canyon.store.completedTodos.get()
-    newList.splice(listId, 1)
-    canyon.store.completedTodos.set(newList.sort())
+    let id = () => "listElement";
+    let uncompleteTodo = () => canyon.actions.uncompleteTodo.bind;
+    let deleteTodo = () => canyon.actions.deleteTodo.bind;
+    let myElement = canyon.template(`<div id="{id}"></div>`, id);
 
-})
-
-canyon.watchers.completedTodoWatcher = canyon.watch([canyon.store.completedTodos], () => {
-
-    let list = canyon.store.completedTodos.get()
-    canyon.store.completedTodoCounter.set(list.length)
-    
-    let id = () => "listElement"
-    let uncompleteTodo = () => canyon.actions.uncompleteTodo.bind
-    let deleteTodo = () => canyon.actions.deleteTodo.bind
-    let myElement = canyon.template(`<div id="{id}"></div>`, id)
-
-    let listItemStyle = () => `display:flex; flex:1; align-items: center;`
-    let strikethrough = () => `text-decoration: line-through; color: grey;`
+    let listItemStyle = () => `display:flex; flex:1; align-items: center;`;
+    let strikethrough = () => `text-decoration: line-through; color: grey;`;
 
     list.forEach((item, i) => {
-        let value = () => item
-        let index = () => i
+      let value = () => item;
+      let index = () => i;
 
-        myElement.appendChild(canyon.template(`
+      myElement.appendChild(
+        canyon.template(
+          `
         <li style="{listItemStyle}" class="list-group-item" data-list-id="{index}">
             <div style="{listItemStyle}{strikethrough}">
                 {value}
@@ -52,8 +55,19 @@ canyon.watchers.completedTodoWatcher = canyon.watch([canyon.store.completedTodos
                     Delete
                 </button>
             </div>
-        </li>`, index, listItemStyle, value, uncompleteTodo, strikethrough, deleteTodo))
-    })
+        </li>`,
+          [
+            index,
+            listItemStyle,
+            value,
+            uncompleteTodo,
+            strikethrough,
+            deleteTodo,
+          ]
+        )
+      );
+    });
 
-    canyon.render(myElement, document.getElementById("completedTodos"))
-})
+    canyon.render(myElement, document.getElementById("completedTodos"));
+  }
+);
--- js/app.js
+++
@@ -1,1 +0,0 @@
-let canyon = new Canyon()
